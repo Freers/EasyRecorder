3d77d4967fd136f59dbae1bdd20ee574cd9fb0fb
lxt <lxt352@gmail.com>
2017-11-30 17:05:09 +0800

    Add video bean

---
+++ app/src/main/java/com/lxt/easyrecorder/demain/Video.kt
@@ -0,0 +1,10 @@
+package com.lxt.easyrecorder.demain
+
+/**
+ * @author lxt <lxt352@gmail.com>
+ * @since 2017/11/30.
+ */
+class Video {
+
+    var path: String? = null
+}
--- app/src/main/java/com/lxt/easyrecorder/view/RecyclerViewAdapter.kt
+++ app/src/main/java/com/lxt/easyrecorder/view/RecyclerViewAdapter.kt
@@ -1,34 +1,38 @@
 package com.lxt.easyrecorder.view
 
+import android.content.Context
 import android.support.v7.widget.RecyclerView
 import android.view.View
 import android.view.ViewGroup
+import com.lxt.easyrecorder.R
+import com.lxt.easyrecorder.demain.Video
+import kotlinx.android.synthetic.main.item_local_video.view.*
 
 /**
  * @author lxt <lxt352></lxt352>@gmail.com>
  * @since 2017/11/30.
  */
+class RecyclerViewAdapter<Video>(private val context: Context, list: List<Video>, private val itemView: View) :
+        RecyclerView.Adapter<RecyclerViewAdapter.ItemViewHolder>() {
 
-class RecyclerViewAdapter<T> : RecyclerView.Adapter<RecyclerViewAdapter.ItemViewHolder<*>>() {
+    private val data: List<Video>? = list
 
-    private val data: List<T>? = null
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder? =
+            ItemViewHolder(context, itemView)
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder<*>? {
-        return null
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+        val get = data?.get(position) as com.lxt.easyrecorder.demain.Video
+        holder.bind(get)
     }
 
-    override fun onBindViewHolder(holder: ItemViewHolder<*>, position: Int) {
+    override fun getItemCount(): Int = data?.size!!
 
-    }
+    class ItemViewHolder(context: Context, itemView: View = View.inflate(context,
+            R.layout.item_local_video, null)) : RecyclerView.ViewHolder(itemView) {
 
-    override fun getItemCount(): Int {
-        return 0
-    }
-
-    class ItemViewHolder<T>(itemView: View) : RecyclerView.ViewHolder(itemView) {
-
-        fun bind(t: T) {
-
+        fun bind(video: Video) {
+            itemView.imageView
+            itemView.title
         }
     }
 }
